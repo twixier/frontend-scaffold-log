b5972468172982b63f5f23e780581bb31a898fdf
Twixier <mhn@dwarf.dk>
2015-11-01 10:29:21 +0100

    Fixed options to provide wide contextual support

--- index.js
+++ index.js
@@ -6,14 +6,42 @@ var util = require('gulp-util');
 const PLUGIN_NAME = "Majestix";
 
 function log (options) {
+  var title,
+      message,
+      statusColor = 'green';
 
-  if(!options.message) {
-    return false;
+  // Check if its only a string that has been passed, if so we assume its a message
+  if(typeof options === "string") {
+    message = options;    
+  } else { 
+    // options.title: 
+    // Check if options.title is set, else overwrite with current plugin_name 
+    if(!options.title) {
+      title = PLUGIN_NAME;
+    } else {
+      title = options.title;
+    }
+
+    if(!options.message) {
+      // If there is no message, we just abort
+      return false;
+    }
+
+    // Check if user has set custom status: error, ok
+    switch(options.status) {
+      case 'ok' || 'OK':
+        statusColor = 'green';
+      break;
+      case 'error' || 'ERROR':
+        statusColor = 'red';
+      break;
+    }
   }
-
-  var template = util.colors.green(PLUGIN_NAME) + ':' + 
+  
+  // Message template 
+  var template = util.colors.cyan(title)+ ':' + 
                  '[' + util.colors.blue(message) + ']',
-                 util.colors['green'].call(util.colors, message);
+                 util.colors[statusColor].call(util.colors, message);
 
   // Log message to console
   util.log(template);
